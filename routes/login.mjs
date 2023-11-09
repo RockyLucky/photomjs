caef74f705c5291fa268663496ef34f896098a71
RockyLucky <noahchatt@gmail.com>
2023-11-09 12:13:17 -0600

    fixed some error

--- routes/login.mjs
+++ routes/login.mjs
@@ -1,7 +1,6 @@
 // routes/login.mjs
 import express from 'express';
 import passport from 'passport';
-import { check, validationResult } from 'express-validator';
 
 const router = express.Router();
 
@@ -9,20 +8,13 @@ router.get('/', (req, res) => {
   res.render('login.ejs');
 });
 
-router.post('/', [
-  check('username').isLength({ min: 5 }),
-  check('password').isLength({ min: 5 })
-], (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
+router.post('/', 
   passport.authenticate('local', {
-    successRedirect: process.env.SUCCESS_REDIRECT || '/',
-    failureRedirect: process.env.FAILURE_REDIRECT || '/login',
+    successRedirect: '/',
+    failureRedirect: '/login',
     failureFlash: true
-  })(req, res, next);
-});
+  })
+);
 
 router.use((err, req, res, next) => {
   console.error(err.stack);
