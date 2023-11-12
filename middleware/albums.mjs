f9ae288bed9d370205f9fa833724af11a165946d
RockyLucky <noahchatt@gmail.com>
2023-11-12 14:30:49 -0600

    Refactor album middleware to refresh album list after create, delete, and rename operations

--- middleware/albums.mjs
+++ middleware/albums.mjs
@@ -12,75 +12,84 @@ let albums = [];
 
 // Define the function to read the albums directory
 const readAlbumsDir = () => {
-  // Read the albums directory
-  fs.readdir(albumsDir, (err, files) => {
-    // If there is an error, log it
-    if (err) {
-      console.log(err);
-    } else {
-      // If there is no error, log the files
-      console.log(files);
-      // Assign the files to the albums array
-      albums = files;
-    }
-  });
+    // Read the albums directory
+    fs.readdir(albumsDir, (err, files) => {
+        // If there is an error, log it
+        if (err) {
+            console.log(err);
+        } else {
+            // If there is no error, log the files
+            console.log(files);
+            // Assign the files to the albums array
+            albums = files;
+        }
+    });
 };
+
+// Call the readAlbumsDir function to load the albums
+readAlbumsDir();
 
 // Define the function to create an album
 const createAlbum = (albumName) => {
-  // Create the album directory
-  fs.mkdir(`${albumsDir}/${albumName}`, (err) => {
-    // If there is an error, log it
-    if (err) {
-      console.log(err);
-    } else {
-      // If there is no error, log the success message
-      console.log(`Album ${albumName} created successfully`);
-    }
-  });
+    // Create the album directory
+    fs.mkdir(`${albumsDir}/${albumName}`, (err) => {
+        // If there is an error, log it
+        if (err) {
+            console.log(err);
+        } else {
+            // If there is no error, log the success message
+            console.log(`Album ${albumName} created successfully`);
+            // Refresh the albums list
+            readAlbumsDir();
+        }
+    });
 };
 
 // Define the function to delete an album
 const deleteAlbum = (albumName) => {
-  // Delete the album directory
-  fs.rmdir(`${albumsDir}/${albumName}`, (err) => {
-    // If there is an error, log it
-    if (err) {
-      console.log(err);
-    } else {
-      // If there is no error, log the success message
-      console.log(`Album ${albumName} deleted successfully`);
-    }
-  });
+    // Delete the album directory
+    fs.rmdir(`${albumsDir}/${albumName}`, (err) => {
+        // If there is an error, log it
+        if (err) {
+            console.log(err);
+        } else {
+            // If there is no error, log the success message
+            console.log(`Album ${albumName} deleted successfully`);
+            // Refresh the albums list
+            readAlbumsDir();
+        }
+    });
 };
 
 
 // Define the function to rename an album
 const renameAlbum = (oldAlbumName, newAlbumName) => {
-  // Rename the album directory
-  fs.rename(`${albumsDir}/${oldAlbumName}`, `${albumsDir}/${newAlbumName}`, (err) => {
-    // If there is an error, log it
-    if (err) {
-      console.log(err);
-    } else {
-      // If there is no error, log the success message
-      console.log(`Album ${oldAlbumName} renamed to ${newAlbumName} successfully`);
-    }
-  });
+    // Rename the album directory
+    fs.rename(`${albumsDir}/${oldAlbumName}`, `${albumsDir}/${newAlbumName}`, (err) => {
+        // If there is an error, log it
+        if (err) {
+            console.log(err);
+        } else {
+            // If there is no error, log the success message
+            console.log(`Album ${oldAlbumName} renamed to ${newAlbumName} successfully`);
+            // Refresh the albums list
+            readAlbumsDir();
+        }
+    });
 };
 
 // Define the function to read the album contents
 const readAlbumContents = (albumName) => {
-  // Read the album directory
-  fs.readdir(`${albumsDir}/${albumName}`, (err, files) => {
-    // If there is an error, log it
-    if (err) {
-      console.log(err);
-    } else {
-      // If there is no error, log the files
-      console.log(files);
-    }
-  });
+    // Read the album directory
+    fs.readdir(`${albumsDir}/${albumName}`, (err, files) => {
+        // If there is an error, log it
+        if (err) {
+            console.log(err);
+        } else {
+            // If there is no error, log the files
+            console.log(files);
+        }
+    });
 };
 
 export { albums, readAlbumsDir, createAlbum, deleteAlbum, renameAlbum, readAlbumContents };
